1660400ab4656d00c3c622579d96146709ed399e
ackarlse <ann.c.karlsen@hiof.no>
2022-09-21 13:14:16 +0200

    responsivt design

---
+++ css-grid/javascript/ham.js
@@ -0,0 +1,10 @@
+const menu = document.getElementById("main-menu");
+
+const hamMenu = () =>{
+    if(menu.style.display == "block"){
+        menu.style.display = "none";
+    }
+    else{
+        menu.style.display = "block";
+    }
+}
